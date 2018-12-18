3eeffef3f32c7ecaad7dcae911e9b1df49fef809
CharityQuinn <charityaquinn@gmail.com>
2018-12-17 20:25:19 -0500

    Updated the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,69 @@
 var userCount = 0;
 var randomNumber = 0;
-var plantNumber = 0;
-var p1 = 0;
-var p2 = 0;
-var p3 = 0;
-var p4 = 0;
+var currentScore = 0;
 
 
+var plants = {
+  bamboo: {
+    name: "bamboo",
+    value: 0
+  },
+  airplant: {
+    name: "airplant",
+    value: 0
+  },
+  jade: {
+    name: "jade",
+    value: 0
+  },
+  spider: {
+    name: "spider",
+    value: 0
+  }
+}
+// setting up a method to retrieve a random numnber
+plants.bamboo.value = random();
+plants.jade.value = random();
+plants.spider.value = random();
+plants.airplant.value = random();
 
+// Set up a start for the game; create a function
 
+function addValue(clickedPlant) {
+  currentScore += clickedPlant.value;
 
-function plant1() {
-  p1 = (Math.floor(Math.random() * 12) + 1);
-  //document.onkeyup = function (userCount)
+  $("#your-score").text(currentScore)
+}
+
+function random() {
+  return Math.floor(Math.random() * 12) + 1;
+
 }
 
 
-function plant2() {
-  p2 = (Math.floor(Math.random() * 12) + 1);
-}
-
-function plant3() {
-  p3 = (Math.floor(Math.random() * 12) + 1);
-}
-
-function plant4() {
-  p4 = (Math.floor(Math.random() * 12) + 1);
-}
 
 function listen() {
   document.onkeyup = userCount;
 
 }
 
+function startGame() {
+  // get a random number to populate the goal number
+  randomNumber = randomTotal();
+  plants.bamboo.value = random();
+  plants.jade.value = random();
+  plants.spider.value = random();
+  plants.airplant.value = random();
+}
+
+// create function to capture clicks for the pictures.
+//add the addValue to it and set it equal to pics
+
+
+
 function randomTotal() {
   randomNumber = (Math.floor(Math.random() * 120) + 19);
+  return Math.floor(Math.random() * 120) + 19;
 
   function userCount() {
     userCount = (p1 + p2 + p3 + p4);
@@ -57,11 +86,9 @@ function randomTotal() {
   }
 
 }
-
-
 //function reset() {
-  wins = 0;
-  losses = 0;
- // $("#clear").on("click", function (event) {
-  //      $("#display").empty();
- //     }
+wins = 0;
+losses = 0;
+// $("#clear").on("click", function (event) {
+//      $("#display").empty();
+//     }
