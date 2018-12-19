ac499b5bcbbda599636ac9947209da3c80f1abd1
CharityQuinn <charityaquinn@gmail.com>
2018-12-18 20:14:29 -0500

    Close but not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,89 +2,98 @@ var userCount = 0;
 var randomNumber = 0;
 var currentScore = 0;
 
+var ball = 0;
+var ice = 0;
+var rocks = 0;
+var orangeIce = 0;
+var randomTotal = 0;
 
-var plants = {
-  bamboo: {
-    name: "bamboo",
-    value: 0
-  },
-  airplant: {
-    name: "airplant",
-    value: 0
-  },
-  jade: {
-    name: "jade",
-    value: 0
-  },
-  spider: {
-    name: "spider",
-    value: 0
-  }
 
+
+
+
+
+function gameTotal() {
+
+  return Math.floor(Math.random() * 120) + 19;
 }
+$(".card-text").textContent = randomNumber;
 
-function random () {
+
+function random() {
   return Math.floor(Math.random() * 12) + 1;
 }
 
 
 function startGame() {
   // get a random number to populate the goal number
-// setting up a method to retrieve a random numnber for plants
-  randomNumber = randomTotal();
-  plants.bamboo.value = random();
-  plants.jade.value = random();
-  plants.spider.value = random();
-  plants.airplant.value = random();
+  // setting up a method to retrieve a random numnber for plants
+
+  randomNumber = gameTotal();
+  ball = random();
+  orangeIce = random();
+  rocks = random();
+  ice = random();
+
 }
 
 // create function to capture clicks for the pictures.
 //add the addValue to it and set it equal to pics
-function clickedPlant () {
-  document.getElementById("random").onclick = value;
-  console.log("This is value for bamboo " + value);
-  userCount = userCount + value;
-  document.getElementById("random").onclick = value;
-  console.log("This is value for jade " + value);
-  userCount = userCount + value;
-  document.getElementById("random").onclick = value;
-  console.log("This is value for spider " + value);
-  userCount = userCount + value;
-  document.getElementById("random").onclick = value;
-  console.log("This is value for airplant " + value);
-  userCount = userCount + value;
+function clickedCrystal() {
+
+  userCount = (ball + orangeIce + rocks + ice)
+
+  //$(#your - score) = userCount;
+  console.log(userCount);
+  console.log(this);
+  return this();
+
 }
+console.log(userCount);
+document.getElementById("ball").addEventListener("click", startGame(ball));
+document.getElementById("ice").addEventListener("click", startGame(ice));
+document.getElementById("orangeIce").addEventListener("click", startGame(orangeIce));
+document.getElementById("rocks").addEventListener("click", startGame(rocks));
+$userCount.innerHTML = clickedCrystal();
 
 
-function randomTotal() {
-  randomNumber = (Math.floor(Math.random() * 120) + 19);
-  console.log("This is the target number " + randomNumber);
-  return Math.floor(Math.random() * 120) + 19;
-}
-  // function userCount() {
-  //   userCount = (p1 + p2 + p3 + p4);
-  //   if (userCount < randomNumber) {
-  //     listen();
-
-  //     if (userCount = randomNumber) {
-  //       wins++;
-  //       //Help  $("#clear").on("click", function (reset);
-
-  //       // send alert saying you!
-
-  //     } else if (userCount > randomNumber) {
-  //       // reset
-  //       // send alert saying you lost!
-  //       losses++;
-  //     }
-//}  
-
-  
+console.log("This is ball's total " + ball);
+console.log("This is ice's total " + ice);
+console.log("This is orangeIce's total " + orangeIce);
+console.log("This is rock's total " + rocks);
+console.log("This is randomNumber's total " + randomNumber);
+console.log("This is userCount's total " + userCount);
 
 
-//function reset() {
-wins = 0;
-losses = 0;
-// $("#clear").on("click", function (event) {
-//      $("#display").empty();
-// }
+
+function userCount() {
+  userCount = (p1 + p2 + p3 + p4);
+  if (userCount < randomNumber) {
+    $("#your-score").textContent = randomNumber;
+    clickedCrystal();
+
+    if (userCount === randomNumber) {
+      wins++;
+     alert("You have won!")
+
+
+      }
+      else if (userCount > randomNumber) {
+        reset()
+        alert("You have lost.")
+
+        losses++;
+      }
+      $("#your-score").textContent = randomNumber;
+    }
+  }
+
+
+
+  function reset() {
+    wins = 0;
+    losses = 0;
+    $("#clear").on("click", function (event) {
+      $("#display").empty();
+    })
+  }
