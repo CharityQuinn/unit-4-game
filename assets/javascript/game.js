b3339388f63077c8347c3e418d9a09d9a0f9f7f2
CharityQuinn <charityaquinn@gmail.com>
2018-12-22 17:59:38 -0500

    Revised but still working on small issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,83 +33,80 @@ ball = random();
 orangeIce = random();
 rocks = random();
 ice = random();
-getClicks();
-  console.log("This is ball's value " + ball);
-  console.log("This is ice's value " + ice);
-  console.log("This is orangeIce's value " + orangeIce);
-  console.log("This is rock's value " + rocks);
-  console.log("This is randomNumber's total " + randomNumber);
+
+
+console.log("This is ball's value " + ball);
+console.log("This is ice's value " + ice);
+console.log("This is orangeIce's value " + orangeIce);
+console.log("This is rock's value " + rocks);
+console.log("This is randomNumber's total " + randomNumber);
+listen();
+
 
 
 // create function to capture clicks for the pictures.
-function getClicks() {
-  console.log("Entering getClicks")
-  document.getElementById("ball").onclick = function() {processClicks("ball")};
-  document.getElementById("orangeIce").onclick = function() {processClicks("orangeIce")};
-  document.getElementById("rocks").onclick = function() {processClicks("rocks")};
-  document.getElementById("ice").onclick = function() {processClicks("ice")};
+function listen() {
+  console.log("In listen, waiting for click")
+  document.getElementById("#ball").onclick = processClicks(ball);
+  
+  document.getElementById("#orangeIce").onclick = processClicks("orangeIce");
+  
+  document.getElementById("#rocks").onclick = processClicks(rocks);
+  document.getElementById("#ice").onclick = processClicks(ice);
 }
- function processClicks () {
-   var x;
-  if (x === "ball") {
-    console.log("processClicks = " + ball);
-    console.log("x = " + x   +"calling userNumber");
-     userNumber (ball);
-  }
-  if (x === "orangeIce") {
-    console.log("processClicks = " + orangeIce);
-    userNumber (orangeIce);
-  }
-  if (x === "rocks") {
-    console.log("processClicks = " + rocks);
-    userNumber(rocks);
-  }
-  if (x === "ice") {
-    console.log("processClicks = " + ice);
-    userNumber(ice);
+
+
+function processClicks() {
+  var x;
+  if (x === ball) {
+    userTotal = userTotal + ball;
+    $("#your-score").html("Your total is: " + userTotal);
+    evalualt(userTotal);
   }
 
 
- }
-function userNumber() {
+  if (x === ice) {
+    userTotal = userTotal + ice;
+    $("#your-score").html("Your total is: " + userTotal);
+    evaluate(userTotal);
+  }
 
-  $("#your-score").html("Your total is: " + userTotal);
-  userTotal = (ball + ice + orangeIce + rocks);
-  $("#your-score").html("Your total is: " + userTotal);
 
+  if (x === orangeIce) {
+    userTotal = userTotal + orangeIce;
+    $("#your-score").html("Your total is: " + userTotal);
+    evaluate(userTotal);
+  }
+
+
+  if (x === rocks) {
+    userTotal = userTotal + rocks;
+    $("#your-score").html("Your total is: " + userTotal);
+    evaluate(userTotal);
+  }
+
+}
+
+
+function evaluate() {
   if (userTotal = 0) {
-  console.log("Press a picture to get started");
+    console.log("Press a picture to get started");
+    listen();
   } else if (userTotal < randomNumber) {
-
     console.log("Inside the count user has " + userTotal + " They need to get to " + randomNumber);
-    if (userTotal < randomNumber) {
-      getClicks ();
-    }
-    
+    listen();
   }
   if (userTotal === randomNumber) {
     wins++;
     alert("You have won!")
   } else if (userTotal > randomNumber) {
-    reset()
     alert("You have lost.")
-
     losses++;
+    reset();
   }
 
 }
 
-
-
-//add the addValue to it and set it equal to pics
-function clickedCrystal() {
-
-  userCount = (ball + orangeIce + rocks + ice)
-  $("#your-score").html("Your total is: " + userCount);
-  console.log("This is the user's total inside clickedCrystal" + userCount);
-  console.log("Sending userCount to function userNumber");
-  userNumber(userCount);
-}
 
 
 function reset() {
