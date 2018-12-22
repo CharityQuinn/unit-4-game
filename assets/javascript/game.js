24fdc6511a27701f7731b7fc022ea6ced0dd50b3
CharityQuinn <charityaquinn@gmail.com>
2018-12-22 12:10:19 -0500

    Working on a revision

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,99 +1,121 @@
 var userCount = 0;
 var randomNumber = 0;
 var currentScore = 0;
-
+var userTotal = 0;
+var wins = 0;
+var losses = 0;
 var ball = 0;
 var ice = 0;
 var rocks = 0;
 var orangeIce = 0;
 var randomTotal = 0;
 
+console.log("Javascript page started");
 
+gameTotal();
 
-
-
-
+// get a random number to populate the goal number
 function gameTotal() {
-
-  return Math.floor(Math.random() * 120) + 19;
+  randomNumber = Math.floor((Math.random() * 120) + 19);
+  console.log("This is the number to get " + randomNumber);
+  $(".card-text").html("<str>Number to get to is: </str>" + randomNumber);
+  $("#your-score").html("Your total is: " + userTotal);
 }
-$(".card-text").textContent = randomNumber;
 
 
+// setting up a method to retrieve a random numnber for plants
 function random() {
-  return Math.floor(Math.random() * 12) + 1;
+  return Math.floor((Math.random() * 12) + 1);
 }
 
+console.log("This is the gameTotal " + randomNumber);
+ball = random();
+orangeIce = random();
+rocks = random();
+ice = random();
+getClicks();
+  console.log("This is ball's value " + ball);
+  console.log("This is ice's value " + ice);
+  console.log("This is orangeIce's value " + orangeIce);
+  console.log("This is rock's value " + rocks);
+  console.log("This is randomNumber's total " + randomNumber);
 
-function startGame() {
-  // get a random number to populate the goal number
-  // setting up a method to retrieve a random numnber for plants
-
-  randomNumber = gameTotal();
-  ball = random();
-  orangeIce = random();
-  rocks = random();
-  ice = random();
-
-}
 
 // create function to capture clicks for the pictures.
+function getClicks() {
+  console.log("Entering getClicks")
+  document.getElementById("ball").onclick = function() {processClicks("ball")};
+  document.getElementById("orangeIce").onclick = function() {processClicks("orangeIce")};
+  document.getElementById("rocks").onclick = function() {processClicks("rocks")};
+  document.getElementById("ice").onclick = function() {processClicks("ice")};
+}
+ function processClicks () {
+   var x;
+  if (x === "ball") {
+    console.log("processClicks = " + ball);
+    console.log("x = " + x   +"calling userNumber");
+     userNumber (ball);
+  }
+  if (x === "orangeIce") {
+    console.log("processClicks = " + orangeIce);
+    userNumber (orangeIce);
+  }
+  if (x === "rocks") {
+    console.log("processClicks = " + rocks);
+    userNumber(rocks);
+  }
+  if (x === "ice") {
+    console.log("processClicks = " + ice);
+    userNumber(ice);
+  }
+
+
+ }
+function userNumber() {
+
+  $("#your-score").html("Your total is: " + userTotal);
+  userTotal = (ball + ice + orangeIce + rocks);
+  $("#your-score").html("Your total is: " + userTotal);
+
+  if (userTotal = 0) {
+  console.log("Press a picture to get started");
+  } else if (userTotal < randomNumber) {
+
+    console.log("Inside the count user has " + userTotal + " They need to get to " + randomNumber);
+    if (userTotal < randomNumber) {
+      getClicks ();
+    }
+    
+  }
+  if (userTotal === randomNumber) {
+    wins++;
+    alert("You have won!")
+  } else if (userTotal > randomNumber) {
+    reset()
+    alert("You have lost.")
+
+    losses++;
+  }
+
+}
+
+
+
 //add the addValue to it and set it equal to pics
 function clickedCrystal() {
 
   userCount = (ball + orangeIce + rocks + ice)
-
-  //$(#your - score) = userCount;
-  console.log(userCount);
-  console.log(this);
-  return this();
-
+  $("#your-score").html("Your total is: " + userCount);
+  console.log("This is the user's total inside clickedCrystal" + userCount);
+  console.log("Sending userCount to function userNumber");
+  userNumber(userCount);
 }
-console.log(userCount);
-document.getElementById("ball").addEventListener("click", startGame(ball));
-document.getElementById("ice").addEventListener("click", startGame(ice));
-document.getElementById("orangeIce").addEventListener("click", startGame(orangeIce));
-document.getElementById("rocks").addEventListener("click", startGame(rocks));
-$userCount.innerHTML = clickedCrystal();
 
 
-console.log("This is ball's total " + ball);
-console.log("This is ice's total " + ice);
-console.log("This is orangeIce's total " + orangeIce);
-console.log("This is rock's total " + rocks);
-console.log("This is randomNumber's total " + randomNumber);
-console.log("This is userCount's total " + userCount);
-
-
-
-function userCount() {
-  userCount = (p1 + p2 + p3 + p4);
-  if (userCount < randomNumber) {
-    $("#your-score").textContent = randomNumber;
-    clickedCrystal();
-
-    if (userCount === randomNumber) {
-      wins++;
-     alert("You have won!")
-
-
-      }
-      else if (userCount > randomNumber) {
-        reset()
-        alert("You have lost.")
-
-        losses++;
-      }
-      $("#your-score").textContent = randomNumber;
-    }
-  }
-
-
-
-  function reset() {
-    wins = 0;
-    losses = 0;
-    $("#clear").on("click", function (event) {
-      $("#display").empty();
-    })
-  }
+function reset() {
+  wins = 0;
+  losses = 0;
+  $("#clear").on("click", function (event) {
+    $("#display").empty();
+  })
+}
